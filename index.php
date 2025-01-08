9a50a61315370f4799118dd2705a770ec728d769
Olivier <olivier@vertuoz.fr>
2025-01-08 09:59:56 +0100

    Init

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ ini_set('display_errors', 1);
     <head>
         <meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <title>Anti Scraping Example</title>
+        <title>Anti Scraping Fonts Example</title>
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 128 128%22><text y=%221.2em%22 font-size=%2296%22>⚫️</text></svg>">
         <link href="css/styles.css" rel="stylesheet">
         <?PHP 
@@ -21,11 +21,11 @@ ini_set('display_errors', 1);
         ?>
     </head>
     <body>
-        <h1>Anti Scraping Example</h1>
+        <h1>Anti Scraping Fonts Example</h1>
 
-        <p>Je souhaite cacher ce numéro : <?PHP echo encrypt_text("0601020304", "Roboto-Black"); ?></p>
+        <p>I want to hide this phone number : <?PHP echo encrypt_text("0601020304", "Roboto-Black"); ?></p>
 
-        <p>Et cet adresse email j'y tiens encore plus : <?PHP echo encrypt_text("adresseemail@supersecrete.com", "Roboto-Medium", 5); ?></p>
+        <p>And also this email with a better encryption : <?PHP echo encrypt_text("adresseemail@supersecrete.com", "Roboto-Medium", 5); ?></p>
     </body>
 </html>
 
